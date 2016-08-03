845152f9069329f56a072e40575e2d7af8c2a992
Thomas Drevon <thomas@bengler.no>
2016-08-03 13:51:01 +0200

    Moar

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # instasift
 Sifting instagram users
+
+
+## todo
+
+o Antall followers
+o Antall de følger
+o Privat account: yes/no
+- Avg. likes
+- Avg. video views
+- Bilde med flest likes
+- Bilde med færrest likes.
+- Avg. post per mnd. Med utgangspunkt i siste 3 mnd.
+- Bad hashtags som  #likeforlike, #instalike, #followme, #followforfollow
+- navnet på instabrukere som har likt et bilde
--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,8 @@
 const path = require('path')
 const username = process.argv[2]
 import {getAccountStats} from 'instagram-scrape-account-stats'
+import {InstagramPosts} from 'instagram-screen-scrape'
+import streamToPromise from 'stream-to-promise'
 
 if (!username) {
   console.log('Usage: instasift <username>')
@@ -14,8 +16,34 @@ function getUserProfile(username) {
   })
 }
 
+function getUserPosts(username) {
+  const streamOfPosts = new InstagramPosts({username: username})
+  const posts = []
+  streamOfPosts.on('data', post => {
+    var time = new Date(post.time * 1000);
+    console.log([
+      username + "'s post from ",
+      time.toLocaleDateString(),
+      " got ",
+      post.likes,
+      " like(s), and ",
+      post.comments,
+      " comment(s)"
+    ].join(''))
+    posts.push(post)
+  })
 
-getUserProfile(username).then(result => {
-  console.log(result)
-  console.log('all done')
+  return streamToPromise(streamOfPosts).then(() => {
+    return posts
+  })
+}
+
+
+// getUserProfile(username).then(result => {
+//   console.log(result)
+//   console.log('all done')
+// })
+
+getUserPosts(username).then(result => {
+  console.log('all done', result.length)
 })
